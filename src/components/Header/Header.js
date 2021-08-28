3240f6ea3c8ec2b0235de66114c3ac7e6dca1a99
TranHoa1 <tranhoahb99@gmail.com>
2021-08-28 14:17:11 +0700

    add firttime

---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <div>
+            <ul>
+                <li>
+                    Home
+                </li>
+                <li>
+                    AboutUs
+                </li>
+                <li>
+                    Contact
+                </li>
+                <li>
+                    Blog
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Header;
